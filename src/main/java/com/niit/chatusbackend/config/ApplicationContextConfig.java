0f787d27f3cbd44caf774100217be28a56a579b2
pradeep <raghavarapupradeep@gmail.com>
2016-12-26 18:38:14 +0530

    ChatusBackend

---
+++ src/main/java/com/niit/chatusbackend/config/ApplicationContextConfig.java
@@ -0,0 +1,62 @@
+package com.niit.chatusbackend.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+
+
+public class ApplicationContextConfig {
+
+	
+	@Bean(name = "dataSource")
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("oracle.jdbc.driver.OracleDrive");
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		dataSource.setUsername("pradeepdeepu");
+		dataSource.setPassword("pradeep");
+	
+		System.out.println("Datasource");
+		return dataSource;
+
+	}
+
+	private Properties getHibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.dialect", "org.hibernate.dialect.OracleDialect");
+		properties.put("hibernate.hbm2ddl.auto", "update");
+		System.out.println("Hibernate Properties");
+		return properties;
+
+	}
+
+	@Autowired
+	@Bean(name = "sessionFactory")
+	public SessionFactory getSessionFactory(DataSource dataSource) {
+		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+		sessionBuilder.addProperties(getHibernateProperties());
+	
+	
+		System.out.println("Session");
+		
+		return sessionBuilder.buildSessionFactory();
+		
+	}
+
+	@Autowired
+	@Bean(name = "transactionManager")
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+		System.out.println("Transaction");
+		return transactionManager;
+	}
+
+			}
