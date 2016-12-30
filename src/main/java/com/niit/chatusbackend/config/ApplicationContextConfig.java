3916f7c4e279214fbbab1ffac8a885e0c05e745d
pradeep <raghavarapupradeep@gmail.com>
2016-12-30 18:11:53 +0530

    ChatusBackend

--- src/main/java/com/niit/chatusbackend/config/ApplicationContextConfig.java
+++ src/main/java/com/niit/chatusbackend/config/ApplicationContextConfig.java
@@ -7,10 +7,21 @@ import javax.sql.DataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.niit.chatusbackend.dao.UserDAO;
+import com.niit.chatusbackend.dao.impl.UserDAOImpl;
+import com.niit.chatusbackend.model.User;
+
+
+@Configuration
+@ComponentScan("com.niit.chatusbackend")
+@EnableTransactionManagement
 
 public class ApplicationContextConfig {
 
@@ -18,7 +29,7 @@ public class ApplicationContextConfig {
 	@Bean(name = "dataSource")
 	public DataSource getDataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("oracle.jdbc.driver.OracleDrive");
+		dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
 		dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
 		dataSource.setUsername("pradeepdeepu");
 		dataSource.setPassword("pradeep");
@@ -31,7 +42,7 @@ public class ApplicationContextConfig {
 	private Properties getHibernateProperties() {
 		Properties properties = new Properties();
 		properties.put("hibernate.show_sql", "true");
-		properties.put("hibernate.dialect", "org.hibernate.dialect.OracleDialect");
+		properties.put("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
 		properties.put("hibernate.hbm2ddl.auto", "update");
 		System.out.println("Hibernate Properties");
 		return properties;
@@ -44,7 +55,7 @@ public class ApplicationContextConfig {
 		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
 		sessionBuilder.addProperties(getHibernateProperties());
 	
-	
+		sessionBuilder.addAnnotatedClasses(User.class);
 		System.out.println("Session");
 		
 		return sessionBuilder.buildSessionFactory();
@@ -58,5 +69,16 @@ public class ApplicationContextConfig {
 		System.out.println("Transaction");
 		return transactionManager;
 	}
-
-			}
+	/*@Autowired
+	@Bean(name = "sessionFactory")
+	public SessionFactory getSessionFactory(DataSource dataSource) {
+		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+		sessionBuilder.addProperties(getHibernateProperties());*/
+		
+			
+	@Autowired
+	@Bean(name = "userDAO")
+	public UserDAO getUserDao(SessionFactory sessionFactory) {
+			return new UserDAOImpl(sessionFactory);
+	}
+	}
--- src/main/java/com/niit/chatusbackend/controller/IndexController.java
+++ src/main/java/com/niit/chatusbackend/controller/IndexController.java
@@ -1,14 +1,37 @@
 package com.niit.chatusbackend.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.niit.chatusbackend.dao.UserDAO;
+import com.niit.chatusbackend.model.User;
+
+@RestController
 public class IndexController {
 
-	 @RequestMapping("/")
-	 public String getIndexPage()
+	@Autowired
+	UserDAO userDAO;
+	 @PostMapping(value="/register")
+			 
+	 public ResponseEntity<User> createUser(@RequestBody User user) 
 	 {
-		 return "index";
+		 userDAO.saveOrUpdate(user);
+		 return new ResponseEntity<User>(user,HttpStatus.OK);
 	 }
+	 
+	 @GetMapping(value="/users/")
+	 public ResponseEntity<List<User>>listUser()
+	 {
+		 List<User> users=userDAO.list();
+		 return new ResponseEntity<List<User>>(users,HttpStatus.OK);
+		 
+	 }
+	 
 }
